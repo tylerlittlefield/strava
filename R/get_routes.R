db5a24753d61ea42ac22801b6b5699c2453e5ed1
tyluRp <tylerlittlefield@hey.com>
2022-06-07 21:24:45 -0700

    add more tidy

--- R/get_routes.R
+++ R/get_routes.R
@@ -15,9 +15,32 @@ get_routes <- function(
 ) {
   resource <- sprintf("routes/%s", id)
 
-  strava(
+  resp <- strava(
     resource = resource,
     scope = scope,
     client = client
   )
+
+  structure(resp, class = c("strava_routes", class(resp)))
 }
+
+is.strava_routes <- function(x) {
+  inherits(x, "strava_routes")
+}
+
+#' @rdname tidy
+#' @export
+tidy.strava_routes <- function(x, ...) {
+  list(
+    athlete = tibble::as_tibble(as.list(unlist(x[["athlete"]]))),
+    map = tibble::as_tibble(as.list(unlist(x[["map"]]))),
+    map_urls = tibble::as_tibble(as.list(unlist(x[["map_urls"]]))),
+    segments = x[["segments"]] %>%
+      tibble::enframe("segment", "value") %>%
+      tidyr::unnest_wider("value") %>%
+      tidyr::unnest_longer("start_latlng", indices_include = FALSE) %>%
+      tidyr::unnest_longer("end_latlng", indices_include = FALSE)
+  )
+}
+
+
