4360fcfb454ac5525599fd59345432bc98625163
tyluRp <tylerlittlefield@hey.com>
2022-06-13 12:49:22 -0700

    more tidy stuff

--- R/get_routes_export_gpx.R
+++ R/get_routes_export_gpx.R
@@ -1,6 +1,7 @@
 #' Export Route GPX
 #'
-#' Returns a GPX file of the route. Requires read_all scope for private routes.
+#' Returns a GPX file of the route. Requires `read_all` scope for private
+#' routes.
 #'
 #' @param id The identifier of the route.
 #' @param scope Authorization scope.
@@ -11,21 +12,45 @@
 #'
 #' @examples
 #' if (FALSE) {
-#'   get_routes_export_gpx("2964569522013822534")
+#'   gpx <- get_routes_export_gpx("2964569522013822534")
+#'   tidy(gpx)
 #' }
 #'
 #' @export
 get_routes_export_gpx <- function(
     id,
-    scope = "activity:read",
+    scope = "read",
     client = strava_client()
 ) {
   resource <- sprintf("routes/%s/export_gpx", id)
 
-  strava(
+  resp <- strava(
     resource = resource,
     scope = scope,
     client = client,
     resp_body = "xml"
+  )
+
+  structure(resp, class = c("strava_routes_export_gpx", class(resp)))
+}
+
+is.strava_routes_export_gpx <- function(x) {
+  inherits(x, "strava_routes_export_gpx")
+}
+
+#' @rdname tidy
+#' @export
+tidy.strava_routes_export_gpx <- function(x, ...) {
+  x <- xml2::as_list(x)[["gpx"]][["trk"]]
+
+  structure(
+    tibble::tibble(
+      ele = unlist(lapply(x$trkseg, "[[", "ele"), use.names = FALSE),
+      lat = unlist(lapply(x$trkseg, attr, "lat"), use.names = FALSE),
+      lon = unlist(lapply(x$trkseg, attr, "lon"), use.names = FALSE)
+    ),
+    route_name = unlist(x$name),
+    route_link = attr(x$link, "href"),
+    route_type = unlist(x$type)
   )
 }
